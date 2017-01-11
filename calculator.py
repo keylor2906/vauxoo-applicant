5557e4a02027fae36d321b9db6c5e11414663a76
keylor2906 <keylor2906@gmail.com>
2017-01-10 22:38:00 -0600

    comentarios corregidos en py

--- calculator.py
+++ calculator.py
@@ -15,9 +15,9 @@ class CalculatorClass(object):
         """
         # variable donde se guardara la suma de todos los numeros
         resultado = 0
-        #ciclo donde se suman los numeros de la lista
+        # ciclo donde se suman los numeros de la lista
         for num in num_list:
-            #suma del valor a la variable resultado
+            # suma del valor a la variable resultado
             resultado = resultado + num
-        #retorno del resultado de la suma de la lista de numeros
+        # retorno del resultado de la suma de la lista de numeros
         return resultado
--- primes.py
+++ primes.py
@@ -15,11 +15,11 @@ class PrimeClass(object):
         if num_int < 2:
             return False
         else:
-            #Ciclo donde se evaluaran las distintas divisiones del numero
+            # Ciclo donde se evaluaran las distintas divisiones del numero
             for count in range(2, num_int):
-                #Evaluacion del numero
+                # Evaluacion del numero
                 if (num_int % count) == 0:
-                    #En caso de ser divisible entre otro numero, no es primo
+                    # En caso de ser divisible entre otro numero, no es primo
                     return False
-            #En caso de no ser divisible entre otro numero, es primo
+            # En caso de no ser divisible entre otro numero, es primo
             return True
