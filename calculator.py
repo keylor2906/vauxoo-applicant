10ef38510d3bea3c762759955632d34f28f2d61c
keylor2906 <keylor2906@gmail.com>
2017-01-10 20:54:54 -0600

    py2

--- calculator.py
+++ calculator.py
@@ -10,7 +10,14 @@ class CalculatorClass(object):
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        Metodo que recibe de entrada una lista de numeros
+        y regresa la suma de estos
         """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        # variable donde se guardara la suma de todos los numeros
+        resultado = 0
+        #ciclo donde se suman los numeros de la lista
+        for num in num_list:
+            #suma del valor a la variable resultado
+            resultado = resultado + num
+        #retorno del resultado de la suma de la lista de numeros
+        return resultado
